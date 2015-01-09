2455f14ba279fd7dc21b73372200a1f97853d63d
KangLin <kl222@126.com>
2015-01-09 20:08:07 +0800

    add unix program

--- travis/install-build-depends.sh
+++ travis/install-build-depends.sh
@@ -2,6 +2,9 @@
 set -ev
 sudo apt-get update
 if [ "windows" = "$TARGET_OS" ]; then
-    sudo apt-get install gcc-mingw-w64
-    sudo apt-get install mingw-w64 
+    sudo apt-get install mingw-w64-i686-dev
+    sudo apt-get install mingw-w64-common 
+    sudo apt-get install mingw-w64 mingw-w64-tools
+else
+    sudo apt-get install xorg-dev 
 fi
