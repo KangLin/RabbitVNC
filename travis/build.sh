83875ca208efe935bc985ed3dc224ce62119a937
KangLin <kl222@126.com>
2015-01-10 09:30:59 +0800

    Modify CMakeLists.txt

--- travis/build.sh
+++ travis/build.sh
@@ -5,7 +5,11 @@ mkdir -p build
 cd build
 
 if [ "windows" = "$TARGET_OS" ]; then
-  cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-cross-mingw32-linux.cmake -DUSER_ROOT_PATH=.. -DCMAKE_C_FLAGS=-I`pwd`/../common -DCMAKE_CXX_FLAGS=-I`pwd`/../common -DCOMPILER_PREFIX="i586-mingw32msvc"
+  cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-cross-mingw32-linux.cmake \
+    -DUSER_ROOT_PATH=.. \
+    -DCMAKE_C_FLAGS=-I`pwd`/../common \
+    -DCMAKE_CXX_FLAGS=-I`pwd`/../common \
+    -DCOMPILER_PREFIX="i686-w64-mingw32" #"i586-mingw32msvc"
 else
   cmake ..
 fi
--- travis/install-build-depends.sh
+++ travis/install-build-depends.sh
@@ -3,8 +3,8 @@ set -ev
 sudo apt-get update -qq
 if [ "windows" != "$TARGET_OS" ]; then
     sudo apt-get install xorg-dev
-#else 
-    #sudo apt-get install mingw-w64-i686-dev
-    #sudo apt-get install mingw-w64-common 
-    #sudo apt-get install mingw-w64 mingw-w64-tools
+else 
+    sudo apt-get install g++-mingw-w64-x86-64 binutils-mingw-w64-i686
+    sudo apt-get install gcc-mingw-w64-i686 g++-mingw-w64
+    sudo apt-get install mingw-w64 mingw-w64-tools
 fi
