7f0b836a2de4b18aaf44d01a780cdcfc02c2af28
KangLin <kl222@126.com>
2020-09-23 13:37:13 +0800

    Modify ci

--- ci/build-before.sh
+++ ci/build-before.sh
@@ -3,10 +3,10 @@ set -e
 sudo apt-get update -qq
 if [ "windows" = "$TARGET_OS" ]; then
     sudo apt-get install gcc-mingw-w64-i686 g++-mingw-w64-i686 binutils-mingw-w64-i686
-    sudo apt-get install mingw-w64 mingw-w64-tools mingw-w64-dev
+    sudo apt-get install mingw-w64 mingw-w64-tools mingw-w64-i686-dev
 elif [ "windows64" = "$TARGET_OS" ]; then
     sudo apt-get install g++-mingw-w64-x86-64 gcc-mingw-w64-x86-64 binutils-mingw-w64-x86-64 
-    sudo apt-get install mingw-w64 mingw-w64-tools mingw-w64-dev
+    sudo apt-get install mingw-w64 mingw-w64-tools mingw-w64-x86-64-dev
 else 
     sudo apt-get install xorg-dev
 fi
--- ci/build.sh
+++ ci/build.sh
@@ -6,19 +6,16 @@ cd build_$TARGET_OS
 
 if [ "windows" = "$TARGET_OS" ]; then
   cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-cross-mingw32-linux.cmake \
-    -DUSER_ROOT_PATH=.. \
-    -DCMAKE_C_FLAGS=-I`pwd`/../common \
-    -DCMAKE_CXX_FLAGS=-I`pwd`/../common \
-    -DCOMPILER_PREFIX="i686-w64-mingw32"
+    -DCOMPILER_PREFIX="i686-w64-mingw32" \
+    -DCMAKE_INSTALL_PREFIX=`pwd`/install
 elif [ "windows64" = "$TARGET_OS" ]; then
   cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-cross-mingw32-linux.cmake \
-    -DUSER_ROOT_PATH=.. \
-    -DCMAKE_C_FLAGS=-I`pwd`/../common \
-    -DCMAKE_CXX_FLAGS=-I`pwd`/../common \
-    -DCOMPILER_PREFIX="x86_64-w64-mingw32"
+    -DCOMPILER_PREFIX="x86_64-w64-mingw32" \
+    -DCMAKE_INSTALL_PREFIX=`pwd`/install
 else
-  cmake ..
+  cmake ..  \
+    -DCMAKE_INSTALL_PREFIX=`pwd`/install
 fi
 
-cmake --build . -- VERBOSE=1 -j 4
-
+cmake --build . -- VERBOSE=1 "-j`cat /proc/cpuinfo |grep 'cpu cores' |wc -l`"
+cmake --build . --target install
